e428c502c36aefda2e09099817b7cf6a04653552
siskoweb <sisko4dev@gmail.com>
2023-12-08 17:11:38 +0100

    debugging

--- src/hooks/useTodoList.ts
+++ src/hooks/useTodoList.ts
@@ -1,12 +1,18 @@
 "use client";
-
 import { TaskType } from "@/types";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function useTodoList() {
-  const [tasks, setTasks] = useState<TaskType[]>(
-    JSON.parse(localStorage.getItem("tasks") ?? "[]")
-  );
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+
+  // useEffect runs only on the client side
+  useEffect(() => {
+    // Check if localStorage is available before using it
+    if (typeof window !== "undefined") {
+      const storedTasks = localStorage.getItem("tasks");
+      setTasks(storedTasks ? JSON.parse(storedTasks) : []);
+    }
+  }, []);
 
   const onSubmit = (task: string): void => {
     if (!task) return console.log("task required");
@@ -17,13 +23,21 @@ export default function useTodoList() {
     };
 
     setTasks((prevTasks) => [...prevTasks, newTask]);
-    localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]));
+
+    // Check if localStorage is available before using it
+    if (typeof window !== "undefined") {
+      localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]));
+    }
   };
 
   const onRemoveTask = (id: number): void => {
-    const filtred = tasks.filter((task: TaskType) => task.id != id);
-    setTasks(filtred);
-    localStorage.setItem("tasks", JSON.stringify(filtred));
+    const filtered = tasks.filter((task: TaskType) => task.id !== id);
+    setTasks(filtered);
+
+    // Check if localStorage is available before using it
+    if (typeof window !== "undefined") {
+      localStorage.setItem("tasks", JSON.stringify(filtered));
+    }
   };
 
   return { onSubmit, tasks, onRemoveTask };
